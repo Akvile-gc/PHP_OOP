c361e29d68405df95a92ecc560b0a2b9d1bf0bb3
Akvile-gc <akvile.gvildyte@gmail.com>
2023-01-23 21:49:30 +0200

    2 - done

--- 1st_Calsses/index.php
+++ 1st_Calsses/index.php
@@ -7,3 +7,15 @@ $rectangle = new Rectangle(1, 3);
 var_dump($rectangle->calculateArea());
 var_dump($rectangle->calculatePerimeter());
 var_dump($rectangle->calculateDiagonal());
+
+include '2_number_calculator.php';
+
+//2nd
+$numberCalculator = new NumberCalculator();
+
+echo $numberCalculator->calculateSum() . PHP_EOL; // 0
+$numberCalculator->addNumber(5);
+$numberCalculator->addNumber(7);
+echo $numberCalculator->calculateSum() . PHP_EOL; // 12
+echo $numberCalculator->calculateProduct() . PHP_EOL;
+echo $numberCalculator->calculateAverage() . PHP_EOL;
