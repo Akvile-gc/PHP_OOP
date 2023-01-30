df7afa27c48c257b5b2b6c95a1d5a8a1f2a0e107
Akvile-gc <akvile.gvildyte@gmail.com>
2023-01-30 19:53:24 +0200

    1 - done

--- 5th_Exceptions/1_password_validator.php
+++ 5th_Exceptions/1_password_validator.php
@@ -13,55 +13,78 @@ class PasswordValidator
 
     public function passwordLength()
     {
-        if (strlen($this->password) >= 10) {
-            $this->specialSymbols();
-        } else {
+        if (strlen($this->password) < 10) {
             throw new Exception('Password must be at least 10 symbols long');
         }
     }
 
     public function specialSymbols()
     {
-        if (preg_match_all('/[$&+,:;=?@#|\'<>.-^*()%!]/', $this->password) >= 2) {
-            $this->passwordLetters();
-        } else {
+        if (preg_match_all('/[$&+,:;=?@#|\'<>.-^*()%!]/', $this->password) < 2) {
             throw new Exception('Password must include at least 2 special symbols');
         }
     }
 
     public function passwordLetters()
     {
-        if ((preg_match_all('/[a-z]/', $this->password) >= 1) && (preg_match_all('/[A-Z]/', $this->password) >= 1)) {
-            $this->passwordNumbers();
-        } else {
+        if ((preg_match_all('/[a-z]/', $this->password) < 1) && (preg_match_all('/[A-Z]/', $this->password) < 1)) {
             throw new Exception('Password must include at least 1 Capital and 1 lower cap letter');
         }
     }
 
     public function passwordNumbers()
     {
-        if (preg_match_all('/[0-9]/', $this->password) >= 1) {
-            echo 'Password is valid' . PHP_EOL;
-        } else {
+        if (preg_match_all('/[0-9]/', $this->password) < 1) {
             throw new Exception('Password must include at least 1 number');
         }
     }
 }
 
+//1.1
+
+//try {
+//    $password = new PasswordValidator($argv[1]);
+//    $password->passwordLength();
+//    $password->specialSymbols();
+//    $password->passwordLetters();
+//    $password->passwordNumbers();
+//    echo 'Password is valid' . PHP_EOL;
+//} catch (Exception $exception){
+//    echo 'Validation error: ' . $exception->getMessage();
+//}
+
+
+//1.2
 try {
     $password = new PasswordValidator($argv[1]);
     $password->passwordLength();
-    return ;
+    echo 'Password is valid' . PHP_EOL;
 } catch (Exception $exception){
-//    echo 'Validation error: ' . $exception->getMessage(); //1.1 part
     file_put_contents('./errors.txt', 'Validation error: ' . $exception->getMessage() . PHP_EOL, FILE_APPEND);
-} finally {
-    file_get_contents('./errors.txt');
 }
 
+try {
+    $password->specialSymbols();
+    echo 'Password is valid' . PHP_EOL;
+} catch (Exception $exception){
+    file_put_contents('./errors.txt', 'Validation error: ' . $exception->getMessage() . PHP_EOL, FILE_APPEND);
+}
 
+try {
+    $password->passwordLetters();
+    echo 'Password is valid' . PHP_EOL;
+} catch (Exception $exception){
+    file_put_contents('./errors.txt', 'Validation error: ' . $exception->getMessage() . PHP_EOL, FILE_APPEND);
+}
 
-
+try {
+    $password->passwordNumbers();
+    echo 'Password is valid' . PHP_EOL;
+} catch (Exception $exception){
+    file_put_contents('./errors.txt', 'Validation error: ' . $exception->getMessage() . PHP_EOL, FILE_APPEND);
+} finally {
+    echo file_get_contents('./errors.txt');
+}
 
 /*
 1.1 Parašykite įrankį slaptažodžio stiprumui nustatyti.
