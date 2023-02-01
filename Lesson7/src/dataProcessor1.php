3db5afc5d294e82b8007e1a121a5ab21780628e3
Akvile-gc <akvile.gvildyte@gmail.com>
2023-02-01 22:19:44 +0200

    L7 - done

--- Lesson7/src/dataProcessor1.php
+++ Lesson7/src/dataProcessor1.php
@@ -78,63 +78,118 @@ $categories = [
     ],
 ];
 
-interface DataProcessor
-{
-    public function process(DataEncoderInterface $format, DataOutputHandlerInterface $output): void;
-
-}
 interface DataEncoderInterface
 {
-    public function encode(string $format): void;
+    public function encode(array $format): string;
+}
+class JsonEncoder implements DataEncoderInterface {
+    public function encode(array $data): string
+    {
+        // TODO: Implement encode() method.
+            return json_encode($data, JSON_PRETTY_PRINT);
+    }
+}
+
+class CvsEncoder implements DataEncoderInterface{
+    public function encode(array $format): string
+    {
+        // TODO: Implement encode() method.
+        $result = 'Type,Items'.PHP_EOL;
+        foreach ($format as $item){
+            $result .= $item['type'] . '.';
+            $result .= count($item['items']) . '.';
+            $result .= PHP_EOL;
+        }
+        return $result;
+    }
 }
 
 interface DataOutputHandlerInterface
 {
-    public function output(string $output): void;
+    public function output(string $data): void;
 }
 
-class JsonDataProcessor implements DataProcessor
-//class DataProcessor
+class TerminalOutputHandler implements DataOutputHandlerInterface {
+    public function output(string $data): void {
+        echo $data.PHP_EOL;
+    }
+}
+
+class FileOutputHandler implements DataOutputHandlerInterface
+{
+    public function output(string $data): void
+    {
+        file_put_contents("./processedData.txt", $data);
+    }
+}
+
+class DataProcessor
 {
     public function __construct(private array $data)
-    {
-    }
-
-    public function process(DataEncoderInterface $format, DataOutputHandlerInterface $output): void
-    {
-        $format->encode();
-        if ($format === 'json'){
-            $jsonSerializedData = json_encode($this->data, JSON_PRETTY_PRINT);
-            if ($output === 'file'){
-                file_put_contents('./processedData.json', $jsonSerializedData);
-            } elseif ($output === 'terminal'){
-                print_r($jsonSerializedData);
-            }
+        {
         }
-    }
 
+    public function process(DataEncoderInterface $encoder, DataOutputHandlerInterface $outputWriter): void{
+        $result = $encoder->encode($this->data);
+        $outputWriter->output($result);
+    }
+}
+
+$jsonEncoder = new JsonEncoder();
+$csvEncoder = new CvsEncoder();
+
+$terminalWriter = new TerminalOutputHandler();
+$fileWriter = new FileOutputHandler();
+
+$dataProcessor = new DataProcessor($categories);
+$dataProcessor->process($jsonEncoder, $fileWriter);
+$dataProcessor->process($jsonEncoder, $terminalWriter);
+$dataProcessor->process($csvEncoder, $fileWriter);
+$dataProcessor->process($csvEncoder, $terminalWriter);
+
+
+//1st
+//class DataProcessor
+//
+//    public function __construct(private array $data)
+//    {
+//    }
 //    public function process(string $format, string $output): void
 //    {
+//        if ($format === 'json'){
+//            $result = json_encode($this->data, JSON_PRETTY_PRINT);
+//        } elseif ($format === 'cvs'){
+//            $result = 'Type,Items'.PHP_EOL;
+//            foreach ($this->data as $item){
+//                $result .= $item['type'] . '.';
+//                $result .= count($item['items']) . '.';
+//                $result .= PHP_EOL;
+//            }
+//        }
+//
+//         if ($output === 'file'){
+//             file_put_contents("./processedData.$format", $result);
+//         } elseif ($output === 'terminal'){
+//             echo $result .PHP_EOL;
+//         }
+
 //        if ($format === 'json'){
 //            $jsonSerializedData = json_encode($this->data, JSON_PRETTY_PRINT);
 //            if ($output === 'file'){
 //                file_put_contents('./processedData.json', $jsonSerializedData);
 //            } elseif ($output === 'terminal'){
-//                print_r($jsonSerializedData);
+//                echo $jsonSerializedData;
 //            }
 //        } elseif ($format === 'xml'){
 //            $xmlSerializedData = xmlrpc_encode($this->data);
 //            if ($output === 'file'){
 //                file_put_contents('./processedData.xml', $xmlSerializedData);
 //            } elseif ($output === 'terminal'){
-//                print_r($xmlSerializedData);
+//                echo $xmlSerializedData;
 //            }
 //        }
-//
-//        // encode data to $format (JSON or XML)
-//        // output it to $output (file or terminal)
 //    }
-}
+//}
 
 
 //if ($format === 'xml'){
