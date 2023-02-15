eee9e6ce96f0ee1fd8dc0b6058f711a0a6c2e9b9
Akvile-gc <akvile.gvildyte@gmail.com>
2023-02-15 19:54:13 +0200

    L12 - history not working

--- Lesson12Session/index.php
+++ Lesson12Session/index.php
@@ -1,35 +1,64 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Visit counter:</h3>
 
-$visit_counter = 0;
-$visit_history = [];
+    <div>
+        <?php session_start();?>
+        <?php if (!isset($_SESSION['visit_counter'])): ?>
+            <?php $_SESSION['visit_counter'] = 0; ?>
+        <?php else: ?>
+            <?php $_SESSION['visit_counter']++; ?>
+        <?php endif; ?>
+    </div>
 
-session_start();
+    <p style="font-weight: bold"><?php echo $_SESSION['visit_counter']?></p>
 
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
-} else {
-    $_SESSION['count']++;
-//    $_SESSION['visit_history'] = $_SERVER['REQUEST_URI'];
-    $_SESSION['url'] = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    <br>
 
-    $visit_counter = $_SESSION['count'];
-    $visit_history[] = $_SESSION['url'];
-//    $visit_history[] = $_SESSION['visit_history'];
-}
+    <h3>Visit history:</h3>
 
-$encodedVisits = json_encode($visit_history, JSON_PRETTY_PRINT);
-file_put_contents('./visits.json', $encodedVisits, FILE_APPEND);
+    <div>
+        <?php if (!isset($_SESSION['visit_history'])): ?>
+            <?php $_SESSION['visit_history'] = []; ?>
+        <?php endif; ?>
+        <?php $_SESSION['visit_history'][] = $_SERVER['REQUEST_URI'];?>
+        <?php foreach ($_SESSION['visit_history'] as $url): ?>
+            <p style="font-weight: bold"><?php echo $url . PHP_EOL;?></p>
+        <?php endforeach; ?>
+    </div>
 
-echo $visit_counter;
-//print_r($visit_history);
+</body>
+</html>
 
 
-/*
-Sukurkite naują sesijos kintamąjį visit_history.
-Jame laikomas visų aplankytų svetainės adresų sąrašas (naudokite $_SERVER['REQUEST_URI'])
-Sąrašą visuomet atvaizduokite programos išvestyje.
-Išbandykite programą aplankydami skirtingus adresus (/test, /test2, /news, t.t.).
-
-Užduotis gali neveikti jeigu nesukonfigūruotas visų adresų nukreipimas į index.php failą
-
- */
+<!--session_start();-->
+<!---->
+<!--//visit counter-->
+<!--if (!isset($_SESSION['visit_counter'])) {-->
+<!--    $_SESSION['visit_counter'] = 0;-->
+<!--} else {-->
+<!--    $_SESSION['visit_counter']++;-->
+<!--}-->
+<!--echo 'Visit counter: ' . $_SESSION['visit_counter']. PHP_EOL;-->
+<!---->
+<!---->
+<!--//visit history-->
+<!--if (!isset($_SESSION['visit_history'])) {-->
+<!--    $_SESSION['visit_history'] = [];-->
+<!--}-->
+<!---->
+<!--$_SESSION['visit_history'][] = $_SERVER['REQUEST_URI'];-->
+<!---->
+<!--echo 'Visit history: ' . PHP_EOL;-->
+<!---->
+<!--foreach ($_SESSION['visit_history'] as $url){-->
+<!--    echo $url . PHP_EOL;-->
+<!--}-->
